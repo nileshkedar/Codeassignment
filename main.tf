436967fd8945522de485b495ca438c3ce6fcb4fb
niludevops <131844843+niludevops@users.noreply.github.com>
2023-06-02 22:22:30 +0530

    Create a Terraform script to deploy a VM. The VM should be in public subnet.

---
+++ main.tf
@@ -0,0 +1,91 @@
+To use your IAM credentials to authenticate the Terraform AWS provider, set the AWS_ACCESS_KEY_ID environment variable.
+export AWS_ACCESS_KEY_ID=
+Now, set your secret key.
+export AWS_SECRET_ACCESS_KEY=
+  
+-------------------------------------------------------------------------------------------------------------------
+ 
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "example_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "example-vpc"
+  }
+}
+
+# Create a public subnet within the VPC
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.example_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "example-public-subnet"
+  }
+}
+
+# Create an internet gateway
+resource "aws_internet_gateway" "example_igw" {
+  vpc_id = aws_vpc.example_vpc.id
+
+  tags = {
+    Name = "example-igw"
+  }
+}
+
+# Create a route table
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.example_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.example_igw.id
+  }
+
+  tags = {
+    Name = "example-public-route-table"
+  }
+}
+
+# Associate the route table with the public subnet
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+# Create a security group to allow SSH access
+resource "aws_security_group" "example_sg" {
+  name        = "example-sg"
+  description = "Allow SSH access"
+  vpc_id      = aws_vpc.example_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "example-security-group"
+  }
+}
+
+# Launch an EC2 instance (VM) in the public subnet
+resource "aws_instance" "example_instance" {
+  ami           = "ami-078efad6f7ec18b8a"
+  instance_type = "t2.micro"
+  key_name      = "website.ppk"
+  subnet_id     = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.example_sg.id]
+
+  tags = {
+    Name = "example-vm"
+  }
+}
